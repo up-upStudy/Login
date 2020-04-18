5d4ddc08e65d5e5699f07254c580d4310f924371
up-upStudy <423044123@qq.com>
2020-04-18 08:13:55 +0800

    operate

---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="login_box"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+
+<style scoped lang="less">
+  .login_box{
+    height: 100%;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
+
+Vue.use(VueRouter)
+
+
+
+const router = new VueRouter({
+  routes:[
+    {
+      path:'/',redirect:'/login' 
+    },
+    {
+      path:'/login',
+      component:Login
+    }
+  ]
+})
+
+export default router
